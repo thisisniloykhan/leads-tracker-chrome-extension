edc407d835d51dcc80785f6f17a623fbdc3d666f
Niloy Khan <niloykhan.dev@gmail.com>
2026-04-28 20:53:19 +0600

    update 1
    changed the color palettes.

--- index.js
+++ index.js
@@ -12,12 +12,12 @@ function render(leads) {
   let listItems = "";
   for (let i = 0; i < leads.length; i++) {
     listItems += `
-            <li>
-                <a target='_blank' href='${leads[i]}'>
-                    ${leads[i]}
-                </a>
-            </li>
-        `;
+      <li>
+        <a target='_blank' href='${leads[i]}'>
+          ${leads[i]}
+        </a>
+      </li>
+    `;
   }
   ulEl.innerHTML = listItems;
 }
